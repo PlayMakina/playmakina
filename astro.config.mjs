0684ad14a428670b54fe72d0b0a2533897eb5290
javi <javi@example.com>
2024-01-30 18:44:28 +0100

    base added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://playmakina.github.io',
+	base: '/playmakina',
 	integrations: [mdx(), sitemap()],
 });
