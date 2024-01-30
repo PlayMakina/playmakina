1e8ab4f9591ae772df8144157f0a9124985b2f64
javi <javi@example.com>
2024-01-30 18:18:39 +0100

    base astro.config deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://PlayMakina.github.io',
-	base: '/site',
+	site: 'https://playmakina.github.io',
 	integrations: [mdx(), sitemap()],
 });
