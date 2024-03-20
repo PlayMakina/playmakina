f21bdfd8652e1678a7cc065779c172dd504b1932
javi <javi@example.com>
2024-03-20 14:20:50 +0100

    seo english

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Play Makina';
-export const SITE_DESCRIPTION = 'Tu desarrolladora de videojuegos favorita: actualizaiones, descargas, podcast..';
+export const SITE_DESCRIPTION = 'Indie videogame developer: podcasts, games, music, artwork..';
